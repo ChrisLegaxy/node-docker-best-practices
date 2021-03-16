eb2ce280a8d49653ec89c909299c0609e976acec
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-16 18:01:47 +0700

    chore: modify greeting message

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getGreetingMessage(): object {
+    return {
+      success: true,
+      message: "Welcome to Node with Docker Best Practices By Chris Van/Chris Legaxy",
+      github: "https://github.com/ChrisLegaxy",
+      status: 200
+    };
   }
 }
